de62e3dc182a0b99fac84ec3465ed9190c324cc8
habibajavaid <habibajavaid17@gmail.com>
2025-07-15 12:36:06 -0700

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){ 
+   
+   
+    cout<< "Hello World\n";
+    cout<<"Hello World! Habiba\t";
+    
+    
+    int age;
+    string name;
+    cout << "Enter your name" << endl;
+    cin>>name;
+    cout << "Enter your age" << endl;
+    cin>>age;
+
+    cout << "Hello !"   << name <<    ", you are  : "   << age  <<   "  year  old" << endl;
+
+
+
+    int num1;
+    int num2;
+    cout<<"Enter first number"<<endl;
+    cin>>num1;
+    cout << "Enter second number" << endl;
+    cin>>num2;
+    int sum = num1 + num2;
+    float average = sum / 2;
+    cout << "Sum\n" << sum <<  "\nAverage\n"  << average << endl;
+  
+
+
+
+}
