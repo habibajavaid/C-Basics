7d585ab273602afd157f790a63592735958e3c08
habibajavaid <habibajavaid17@gmail.com>
2025-07-16 06:46:44 -0700

    Add files via upload

---
+++ MultiVariables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      
+    int x,y,z;
+
+
+    x = 90;
+    y = 20;
+    z = 10;
+
+    cout << "x:" << x << "  y:" << y << "  z:" << z <<endl;
+
+    cout << 50 + true << endl;
+    cout << 'A' + 'C' <<endl;
+    cout << 'B' + 'C' <<endl;
+    return 0;
+
+
+}
