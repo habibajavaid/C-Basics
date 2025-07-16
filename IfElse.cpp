58e1c957df10ac6f27abd6600ffd310b7d26ee7f
habibajavaid <habibajavaid17@gmail.com>
2025-07-16 10:17:55 -0700

    Add files via upload

---
+++ IfElse.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+
+/*
+    int marks = 90;
+
+   // cout <<"Enter the marks" <<endl;
+    //cin>>marks;
+   
+    if(marks >= 90){
+        cout << "Grade A\n";
+    }else if(marks >= 80){
+        cout << "Grade B\n";
+    }else if (marks >=  70){
+        cout << "Grade C\n";
+    }else if (marks >= 60){
+        cout << "Grade D\n";
+    }else {
+        cout << "Fail\n";
+    }
+        */
+    
+/*
+        int number;
+
+        cout << "Enter a number\n";
+        cin >> number;
+
+        if(number > 0 ){
+            cout << " The number is positive\n";
+        } else if (number < 0){
+            cout << "The number is negative\n";
+        } else {
+            cout << " The number is invalid\n";j
+        }
+*/
+
+     /*
+ 
+        int num ;
+        cout << "Enter the  number";
+        cin >> num;
+
+        if(num % 2 == 0){
+            cout << "The number is even";
+        }else {
+            cout << "The number is odd\n";
+        }
+            */
+
+            /*
+
+            int age;
+            cout << " Enter your age\n";
+            cin >> age;
+
+            if(age >= 18){
+                cout << "Eligible for vote\n";
+            } else {
+                cout << "You are not eligible for vote\n";
+            }
+                */
+
+                int temperature;
+
+                cout << "Enter the temperature in celsius\n";
+                cin >> temperature;
+
+                if(temperature > 30 ){
+                    cout << "It's hot!\n";
+                } else if (temperature >= 15 && temperature < 30){
+                    cout << "The Normal!\n";
+                } else {
+                    cout << "It's cold!";
+                }
+        }
+
+        
+
