8c38b09fd4fc5f2b1a7120247b462edd015be3d4
habibajavaid <habibajavaid17@gmail.com>
2025-07-16 06:46:11 -0700

    Add files via upload

---
+++ Arithmetic Operation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1 ,num2;
+
+    cout<<"Enter first number" << endl;
+    cin>> num1;
+    cout << "Enter second number" << endl;
+    cin >> num2;
+
+    cout <<"Addition :" <<num1 + num2 <<endl;
+    cout <<"Subtraction :" <<num1 - num2 <<endl;
+    cout <<"Multiplication :" <<num1 * num2 <<endl;
+    cout <<"Division :" <<num1 / num2 <<endl;
+}
