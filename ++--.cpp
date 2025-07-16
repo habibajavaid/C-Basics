d909b9e57e578d55270e68b75381e612adc32794
habibajavaid <habibajavaid17@gmail.com>
2025-07-16 06:47:19 -0700

    Add files via upload

---
+++ ++--.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   
+    int x , y;
+
+    x = 100;
+    y = 200;
+   cout << " Before" << x + y << endl;
+    x++;
+    y;
+     
+    
+  
+    cout << "x:" << x <<" y: " <<y << endl; 
+    cout << " Aftern    " << x++ + y << endl;
+
+    
+}
+
+    
