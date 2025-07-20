bbb6d88c394e2d3f624a82a7cce1ff04d93bbfbb
habibajavaid <habibajavaid17@gmail.com>
2025-07-20 05:38:07 -0700

    Add files via upload

--- Const to int ,int to Const pointers.cpp
+++ Const to int ,int to Const pointers.cpp
@@ -2,7 +2,34 @@
 using namespace std;
 
 int main(){
+    
+    int a = 100;
+    int b = 200;
+   
+/*
+    // pointer to const...
+
+    const int* ptr = &a; //In which we do not change the value of the varible because it's constant and we change the value of the ptr mean variable address....
+    
+    cout << ptr << endl;
+
+         ptr = &b;
+         b = 300;
+         cout << *ptr << endl;   //In which we do , change the value of b because it's not const.
+
+*/
+    // Const to pointer.... (Constant Pointers).....
+
+    int* const  aptr = &a;
+
+       *aptr = 25;                 //In constant pointer we change the value of the variable but do not change their address like ptr not change.
+    cout << *aptr  << endl;
      
+    
+    return 0;
+
+
+    /*
     int savings = 10000;
    const int savings2 = 50000; // do not touch.
 
@@ -17,4 +44,5 @@ int main(){
    // savingsPtr4 = &savings;  don't allow.
     cout << savings << endl;
     cout << savings2 << endl;
+    */
 }
